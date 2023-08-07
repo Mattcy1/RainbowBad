48356954324e87f947d5a40d7e717da20769e963
Mattcy <140085165+Mattcy1@users.noreply.github.com>
2023-08-07 10:04:22 +0200

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,13 @@
+﻿
+namespace RainbowBad;
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "38.1";
+    public const string Version = "1.1";
+    public const string Name = "RainbowBad";
+
+    public const string Description = "Add the rainbow bad to bloon td 6 spawn at round 100";
+
+    public const string RepoOwner = "Mattcy1"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "Rainbow-Bad"; // TODO add your repo name here, also in the download url in README.md
+}
